95c39a2ffc6fd5972d8d3adc1aba0b3c7f5af34f
WilliamZhu <allwefantasy@gmail.com>
2021-05-19 11:42:28 +0800

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 project=mlsql_plugin_tool
-version=0.1.0
+version=0.1.1
 rm -rf ./dist/* && pip uninstall -y ${project}
 python setup.py sdist bdist_wheel
 cd ./dist/
 pip install ${project}-${version}-py3-none-any.whl
-cd -
+cd -g
