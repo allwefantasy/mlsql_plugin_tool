c22c1fb853c54c779973820eb5e28bccde550f79
WilliamZhu <allwefantasy@gmail.com>
2021-09-17 18:23:36 +0800

    update

--- install.sh
+++
@@ -1,7 +0,0 @@
-project=mlsql_plugin_tool
-version=0.1.3
-rm -rf ./dist/* && pip uninstall -y ${project}
-python setup.py sdist bdist_wheel
-cd ./dist/
-pip install ${project}-${version}-py3-none-any.whl
-cd -
--- publish.sh
+++ publish.sh
@@ -1,8 +1,23 @@
+#!/usr/bin/env bash
+
 project=mlsql_plugin_tool
 version=0.1.3
+
+quoteVersion=$(cat version.py|grep "__version__" |awk -F'=' '{print $2}'| xargs )
+
+if [[ "${version}" != "${quoteVersion}" ]];then
+   echo "version[${quoteVersion}] in version.py is not match with version[${version}] you specified"
+   exit 1
+fi
+
+echo "uninstall current local version"
 rm -rf ./dist/* && pip uninstall -y ${project}
 python setup.py sdist bdist_wheel
-#cd ./dist/
-#pip install ${project}-${version}-py3-none-any.whl
-twine upload dist/*
+cd ./dist/
+
+echo "install to local"
+pip install ${project}-${version}-py3-none-any.whl
 cd -
+
+echo "upload"
+twine upload dist/*
